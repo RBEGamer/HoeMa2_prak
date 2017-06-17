b8d3b5e25b6d6b79a85d7c45b8647978071b630e
RBEGamer <marcel.ochsendorf@gmail.com>
2017-06-17 21:30:58 +0200

    abweichung calc finish

--- MATHE_PRAK_4/main.cpp
+++ MATHE_PRAK_4/main.cpp
@@ -42,8 +42,11 @@ vector<complex>  werte_einlesen(char *dateiname)
     std::cout << "values_read:" << werte.size() << std::endl;
     return werte;
 }
-long werte_ausgeben(char *dateiname, vector<complex> werte, double epsilon=-1.0)
+long werte_ausgeben(char *dateiname, vector<complex> werte, double epsilon=-1.0, vector<complex>* werte_exp = nullptr)
 {
+    if(werte_exp != nullptr){
+        werte_exp->clear();
+    }
     long exported = 0;
     int i;
     int N = werte.size();
@@ -61,6 +64,9 @@ long werte_ausgeben(char *dateiname, vector<complex> werte, double epsilon=-1.0)
         if (werte[i].abs() > epsilon)
             exported++;
             fp << i << "\t" << werte[i].get_re() << "\t" << werte[i].get_im() << endl;
+            if(werte_exp != nullptr){
+                werte_exp->push_back(werte[i]);
+            }
     // File schliessen
     fp.close();
     return exported;
@@ -93,29 +99,46 @@ std::vector<complex>fourier(std::vector<complex> _values, FOURIER_MODE _mode = F
 
 int main() {
     //read values A4
-    std::vector<complex> read_values = werte_einlesen("../original.txt");
+    std::vector<complex> original_values = werte_einlesen("../original.txt");
     //write values A4
 
-    long exp_count_10 = werte_ausgeben("../output_10.txt",fourier(read_values,FOURIER_MODE::FORWARD),1.0);
-    long exp_count_01 =werte_ausgeben("../output_01.txt",fourier(read_values,FOURIER_MODE::FORWARD),0.1);
-    std::cout << "es wurden " << exp_count_10 << " werte mit epsilon=1.0 exportiert" << std::endl;
-    std::cout << "es wurden " << exp_count_01 << " werte mit epsilon=0.1 exportiert" << std::endl;
 
-
-
+    std::vector<complex> write_values_10;
+    std::vector<complex> write_values_01;
+    //mache hintransformtation
+    std::cout << "es wurden " <<  werte_ausgeben("../output_10.txt",fourier(original_values,FOURIER_MODE::FORWARD),1.0, &write_values_10) << " werte mit epsilon=1.0 exportiert" << std::endl;
+    std::cout << "es wurden " << werte_ausgeben("../output_01.txt",fourier(original_values,FOURIER_MODE::FORWARD),0.1, &write_values_01) << " werte mit epsilon=0.1 exportiert" << std::endl;
+    //lese werte
     std::vector<complex> read_values_10 = werte_einlesen("../output_10.txt");
     std::vector<complex> read_values_01 = werte_einlesen("../output_01.txt");
-
-    if(exp_count_01 != read_values_01.size()){
-        std::cout << "compare 01 not possible" << std::endl;
+    //mache rücktransformation
+    read_values_10 = fourier(read_values_10,FOURIER_MODE::BACK);
+    read_values_01 = fourier(read_values_01,FOURIER_MODE::BACK);
+    //berechne abweichung für 1.0
+    if(write_values_10.size() != read_values_10.size()){
+        std::cout << "compare 1.0 not possible" << std::endl;
     }
-
+    double abweichung_ep_10 = -1.0f;
+    for (int i = 0; i < write_values_10.size(); ++i) {
+        if((read_values_10[i].abs()-write_values_10[i].abs()) > abweichung_ep_10){
+         abweichung_ep_10 = (read_values_10[i].abs()-write_values_10[i].abs());
+         }
+    }
+    std::cout<< "abweichung 0.1 ist " << abweichung_ep_10 << std::endl;
+    //berechne abweiung für 0.1f
+    if(write_values_01.size() != read_values_01.size()){
+        std::cout << "compare 0.1 not possible" << std::endl;
+    }
     double abweichung_ep_01 = -1.0f;
-    for (int i = 0; i < exp_count_01; ++i) {
-      //  if(read_values_01[i]- > abweichung_ep_01){
-        // abweichung_ep_01 = read_values_01[i]-;
-        // }
+    for (int i = 0; i < write_values_01.size(); ++i) {
+        if((read_values_01[i].abs()-write_values_01[i].abs()) > abweichung_ep_01){
+            abweichung_ep_01 = (read_values_01[i].abs()-write_values_01[i].abs());
+        }
     }
     std::cout<< "abweichung 0.1 ist " << abweichung_ep_01 << std::endl;
+
+
+
+    
     return 0;
 }

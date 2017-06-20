87d58209c92dc4db5e92ec15243efc4cbbed080d
RBEGamer <marcel.ochsendorf@gmail.com>
2017-06-20 16:01:01 +0200

    fixing im export

--- MATHE_PRAK_4/main.cpp
+++ MATHE_PRAK_4/main.cpp
@@ -45,16 +45,16 @@ vector<complex>  werte_einlesen(char *dateiname)
     std::cout << "values_read:" << werte.size() << std::endl;
     return werte;
 }
-long werte_ausgeben(char *dateiname, vector<complex> werte, double epsilon=-1.0, vector<complex>* werte_exp = nullptr) {
-    if (werte_exp != nullptr) {
-        werte_exp->clear();
-    }
+vector<complex> werte_ausgeben(char *dateiname, vector<complex>& werte, double epsilon=-1.0, bool ignore_exp = false) {
+    vector<complex> werte_exp;
+        werte_exp.clear();
+
     long exported = 0;
     int i;
     int N = 0;
 
     for (i = 0; i < werte.size(); i++){
-        if (werte[i].abs() > epsilon) {
+        if (ignore_exp || werte[i].abs() > epsilon) {
         
 		   N++;
         }
@@ -65,30 +65,27 @@ long werte_ausgeben(char *dateiname, vector<complex> werte, double epsilon=-1.0,
     fp.open(dateiname, ios_base::trunc);
     if (fp.fail()) {
         std::cout << "file could not open - export" << std::endl;
-        return -1;
+        return werte_exp;
     }
     // Dimension in das File schreiben
     fp << N << endl;
     // Eintraege in das File schreiben
     if (N < 0) {
         fp.close();
-        return exported;
+        return werte_exp;
 	   
 	  
     }
     for (i = 0; i < werte.size(); i++)
-	   if (werte[i].abs() > epsilon) {
+	   if (ignore_exp || werte[i].abs() > epsilon) {
 		  exported++;
 		  fp << i << "\t" << werte[i].get_re() << "\t" << werte[i].get_im() << endl;
-		  if (werte_exp != nullptr) {
-			 werte_exp->push_back(werte[i]);
-		  }
-		 
+			 werte_exp.push_back(werte[i]);
 	   }
 	
     // File schliessen
     fp.close();
-    return exported;
+    return werte_exp;
 }
 
 //A3
@@ -96,11 +93,6 @@ enum FOURIER_MODE{
     FORWARD,
     BACK
 };
-
-
-
-
-
 
 std::vector<complex> fourier(std::vector<complex> _values, FOURIER_MODE _mode = FOURIER_MODE::FORWARD) {
    
@@ -172,114 +164,12 @@ std::vector<complex> fourier(std::vector<complex> _values, FOURIER_MODE _mode = 
 	 
 	   }
 
-/*
-            int n = _values.size();
-            for (int k = 0; k < n; k++) {  // For each output element
-                double sumreal = 0;
-                double sumimag = 0;
-                for (int t = 0; t < n; t++) {  // For each input element
-                    double angle = 2 * PI * t * k / n;
-                    sumreal +=  _values[t].get_re() * cos(angle) + _values[t].get_im() * sin(angle);
-                    sumimag += -_values[t].get_re() * sin(angle) + _values[t].get_im() * cos(angle);
-                }
-                output_values[k]= complex(sumreal,sumimag);
-            }
-*/
-	  /*long data_size = 512;
-	  long data_size = _values.size();
-
-	  unsigned butterflySize;   // size for actual butterfly calculation
-	  int i, j, k;              // local index variables
-	  complex wActual(0.0,0.0);  // actual rotation factor
-	  complex wStep(0.0);    // step rotation factors
-	  complex tmp(0.0,0.0);      // temp. value for butterfly calculation
-	  // loop over all level of FFT
-	  for(butterflySize=data_size/2; butterflySize>0; butterflySize/=2) {
-		  // evaluate angle step and set first angle
-		  wStep = complex(cos(-PI/butterflySize), sin(-PI/butterflySize));
-		  wActual = complex(1, 0);
-		  // loop over number of butterflys
-		  for(j=0; j<butterflySize; j++) {
-			  // loop over number of FFTs
-			  for(i=j; i<data_size; i+=2*butterflySize) {
-				  // get index of second element
-				  k = i+butterflySize;
-				  // perform butterfly calculation
-				  tmp = output_values[i];          // store one element
-				  output_values[i] =output_values[i] + output_values[k];     // take sum
-				  output_values[k] = tmp-output_values[k];  // take difference
-				  output_values[k] = output_values[k]*wActual;     // multiply with rotation factor
-			  }
-			  // evaluate next rotation factor
-			  wActual = wActual*wStep;
-		  }
-	  }
-	  // perform bit reversal
-	  j = 0;
-	  for(i=0; i<data_size; i++) {
-		  if(j>i) {
-			  // swap numbers
-			  tmp = output_values[i];
-			  output_values[i] = output_values[j];
-			  output_values[j] = tmp;
-		  }
-		  k = data_size/2;
-		  while(k>=2 && j>=k) {
-			  j -= k;
-			  k /= 2; }
-		  j += k; }
-
-
-/*
-	  // reverse-binary reindexing
-	  long nn = data_size;
-	  long n = data_size<<1;
-	  long i = 0;
-	  long j=1;
-	  for (i=1; i<n; i+=2) {
-		  if (j>i) {
-			  swap(_values[j-1], _values[i-1]);
-			  swap(_values[j], _values[i]);
-		  }
-		  long m = nn;
-		  while (m>=2 && j>m) {
-			  j -= m;
-			  m >>= 1;
-		  }
-		  j += m;
-	  };
-*/
-
-/*
-        for (int rek_ebene = 0; rek_ebene < data_size; ++rek_ebene) {
-            long long fft_abschnitte = (long)pow(2.0f,(float)(data_size-rek_ebene-1));
-            for (int abschnitt = 0; abschnitt < fft_abschnitte; ++abschnitt) {
-
-                int fft_element = (int)pow(2.0f,(float)(rek_ebene -1));
-                for (int element = 0; element < fft_element; ++element) {
-
-                    int index_links =(pow(2.0f,(float)(rek_ebene+1))*abschnitt)+element;
-                    int index_rechts = index_links + pow(2.0,rek_ebene);
-
-                    double phi_links =-1.0*PI*(element/rek_ebene);
-                    double  phi_rechts = -1.0*PI*(element/rek_ebene);
-
-                    output_values[index_links] = _values[index_links] + complex(phi_links) *_values[index_rechts];
-                    output_values[index_rechts] = _values[index_links] - complex(phi_rechts) * _values[index_rechts];
-                }
-            }
-        }
-*/
-   
-	  
 	  return c_transformation_values;
 	  
-   }
-   
-   else if (_mode == FOURIER_MODE::BACK) {
+   } else if (_mode == FOURIER_MODE::BACK) {
 	  
-	  std::cout << "FM:FORWARD" << std::endl;
-	  
+	  std::cout << "FM:BACK" << std::endl;
+
 	  std::vector<complex> c_values;
 	  c_values.clear();
 	  for (int i = 0; i < AnzahlFunktionsWerte; ++i) {
@@ -333,7 +223,7 @@ std::vector<complex> fourier(std::vector<complex> _values, FOURIER_MODE _mode = 
 	  return Re_transformation_values;
 }
 	  
-	   //double to complex
+
     else{
         std::cout << "_mode no omplemented" << std::endl;
     }
@@ -346,31 +236,34 @@ std::vector<complex> fourier(std::vector<complex> _values, FOURIER_MODE _mode = 
 int main() {
    
    
-   //read values A4
+   //READ ORIGINAL FILES
     std::vector<complex> original_values = werte_einlesen("../original.txt");
-    //write values A4
-
-    std::vector<complex> write_values_default;
-    std::vector<complex> write_values_10;
-    std::vector<complex> write_values_01;
+    //MAKE FOURIER FORWARD TRANSFORMATION
+    std::vector<complex> fourier_forward = fourier(original_values,FOURIER_MODE::FORWARD);
+    //EXPORT VALUES
+    std::vector<complex> write_values_default = werte_ausgeben("../output_default.txt",fourier_forward,-1.0);
+    std::vector<complex> write_values_10 = werte_ausgeben("../output_10.txt",fourier_forward,1.0);
+    std::vector<complex> write_values_01 = werte_ausgeben("../output_10.txt",fourier_forward,0.1);
     //mache hintransformtation
-    std::cout << "es wurden " << werte_ausgeben("../output_default.txt",fourier(original_values,FOURIER_MODE::FORWARD),-1.0, &write_values_default) << " werte mit epsilon=-1.0 exportiert" << std::endl;
-    std::cout << "es wurden " <<  werte_ausgeben("../output_10.txt",fourier(original_values,FOURIER_MODE::FORWARD),1.0, &write_values_10) << " werte mit epsilon=1.0 exportiert" << std::endl;
-    std::cout << "es wurden " << werte_ausgeben("../output_01.txt",fourier(original_values,FOURIER_MODE::FORWARD),0.1, &write_values_01) << " werte mit epsilon=0.1 exportiert" << std::endl;
+    std::cout << "es wurden " << write_values_default.size() << " werte mit epsilon=-1.0 exportiert" << std::endl;
+    std::cout << "es wurden " <<  write_values_10.size() << " werte mit epsilon=1.0 exportiert" << std::endl;
+    std::cout << "es wurden " << write_values_01.size() << " werte mit epsilon=0.1 exportiert" << std::endl;
     //lese werte
     std::vector<complex> read_values_default = werte_einlesen("../output_default.txt");
     std::vector<complex> read_values_10 = werte_einlesen("../output_10.txt");
     std::vector<complex> read_values_01 = werte_einlesen("../output_01.txt");
     //mache rücktransformation
    
-   std::vector<complex> retrans_values_default;
-   std::vector<complex> retrans_values_10;
-   std::vector<complex> retrans_values_01;
+   std::vector<complex> retrans_values_default = fourier(read_values_default,FOURIER_MODE::BACK);
+   std::vector<complex> retrans_values_10 = fourier(read_values_10,FOURIER_MODE::BACK);
+   std::vector<complex> retrans_values_01 = fourier(read_values_01,FOURIER_MODE::BACK);
    
-   retrans_values_default = fourier(read_values_default,FOURIER_MODE::BACK);
-   retrans_values_10 = fourier(read_values_10,FOURIER_MODE::BACK);
-   retrans_values_01 = fourier(read_values_01,FOURIER_MODE::BACK);
 
+
+    //WRITE BACK TRANS
+    werte_ausgeben("../output_BACK_default.txt",retrans_values_default,-1.0, true);
+    werte_ausgeben("../output_BACK_10.txt",retrans_values_default,1.0, true);
+    werte_ausgeben("../output_BACK_01.txt",retrans_values_default,0.1, true);
 
     //berechne abweichung für -1.0f defualt
     long compare_index_default = write_values_default.size();
